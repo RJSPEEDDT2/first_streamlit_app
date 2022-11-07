71bd0b088e5d41589e6d6796c41b0b56da1d6b65
RJSPEEDDT2 <117459208+RJSPEEDDT2@users.noreply.github.com>
2022-11-07 07:43:39 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
-# write your own comment -what does the next line do? 
+# formats response data so easier to read
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+# displays in tabular format
 streamlit.dataframe(fruityvice_normalized)
