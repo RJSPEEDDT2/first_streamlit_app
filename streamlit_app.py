8a437ebbe24eb95fe7665d716b740b6d88e8bb70
RJSPEEDDT2 <117459208+RJSPEEDDT2@users.noreply.github.com>
2022-11-09 08:23:53 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,22 +22,30 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 streamlit.dataframe(fruits_to_show)
 
+#New Section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else 
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+    # formats response data so easier to read
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # displays in tabular format
+    streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+except URLError as e:
+    streamlit.error()
 
 
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 
-# formats response data so easier to read
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# displays in tabular format
-streamlit.dataframe(fruityvice_normalized)
+
+
 
 streamlit.stop()
 
@@ -53,6 +61,23 @@ streamlit.dataframe(my_data_row)
 fruit_to_add = streamlit.text_input('What fruit would you like to add?','Kiwi')
 streamlit.write('Thanks for adding ', fruit_to_add)
 my_cur.execute("INSERT into fruit_load_list values ('from streamlit')")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 #my_cur = my_cnx.cursor()
